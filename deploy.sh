6147cbf25df699e120597b69891a4f85ac512db9
cforclown <hafisalrizal@gmail.com>
2021-05-29 15:21:22 +0700

    refresh gitignore

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 tar czf prod.tar.gz public src package.json package-lock.json LICENSE
-scp prod.tar.gz 185.201.9.141:~
+scp -i id_rsa prod.tar.gz root@185.201.9.141:~/hafisalrizal.com.prod
 rm prod.tar.gz
